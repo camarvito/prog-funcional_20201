e687d1be1b995af027d176d43f8458ea71f9a123
camarvito <victormacgrath@gmail.com>
2020-03-01 20:17:25 -0300

    fixed folder examples

--- examples/list-compreension.hs
+++
@@ -1,4 +0,0 @@
-myFunc = [x*2 | x <- [1..10]]
-myFunc2 = [x*2 | x <- [1..10], x*2 >= 12]
-myFunc3 = [ x | x <- [50..100], x `mod` 7 == 3]
-boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
